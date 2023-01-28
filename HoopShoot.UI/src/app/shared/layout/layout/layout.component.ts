a2fbe0a535ae7415e2fbe89aa612f8969282240e
Georgi Kostov <g3rg1.kostov@gmail.com>
2023-01-28 16:17:26 +0200

    Add design to layout component

--- HoopShoot.UI/src/app/shared/layout/layout/layout.component.ts
+++ HoopShoot.UI/src/app/shared/layout/layout/layout.component.ts
@@ -1,8 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { TeamsService } from 'src/app/core/services/teams.service';
-import { MatchesService } from 'src/app/core/services/matches.service';
-import { Subject, takeUntil } from 'rxjs';
-import { Match } from '../../models/match.model';
 import { Router } from '@angular/router';
 
 @Component({
@@ -12,21 +8,9 @@ import { Router } from '@angular/router';
 })
 export class LayoutComponent implements OnInit {
   
-  notifier = new Subject<void>;
-
-  matches!: Match[];
-  highlightMatch!: Match;
-  
-  constructor(private teamsService: TeamsService, private matchesService: MatchesService, public readonly router: Router) { }
+  constructor(public readonly router: Router) { }
 
   ngOnInit(): void {
-     this.matchesService.getMatches()
-     .pipe(takeUntil(this.notifier))
-     .subscribe(matches => this.matches = matches);
-
-     this.matchesService.getHighLightMatch()
-     .pipe(takeUntil(this.notifier))
-     .subscribe(match => this.highlightMatch = match);
   }
 
 }
